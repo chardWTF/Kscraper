415d20e8bd4d6d355d38852d5aaab1a6a33bbc2c
chard <chard@azr.tools>
2025-03-30 13:36:37 +0100

    Update send_chat.py

--- examples/send_chat.py
+++ examples/send_chat.py
@@ -4,7 +4,7 @@ from Kscraper import Client  # Import the Client class
 client = Client()
 
 # Call the get_emotes method on the instance
-channel = client.get_channel(username="chard0") # get the channel JSON
+channel = client.get_channel(username="username") # get the channel JSON
 user_id = channel["user_id"]
 response = client.send_chat(user_id ="12345", token="token", content="content") # grab the bearer token from your browser and put in the part after Bearer
 print(response)
